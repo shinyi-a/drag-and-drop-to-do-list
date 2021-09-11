2999ec8fc2e9818df9624c7c337a556125b26673
Shin Yi <contact.shinyi@gmail.com>
2021-09-11 18:19:07 +0800

    third commit

--- todo.js
+++ todo.js
@@ -1,7 +1,31 @@
+const list = [];
+
+const render = () => {
+    $('ul').empty()
+    list.forEach((item) => {
+        $('ul').append(`<li>${item}</li>`)
+    })
+}
+
+
+
 $(()=>{  
     $('#aboutButton').on('click', aboutList);
     $('#closeButton').on('click', closeAboutList);
+
+    $('form').on('submit', (event) => {
+        const input = $('#input-box').val()
+        list.push(input)
+        event.preventDefault();
+        $(event.currentTarget).trigger('reset');
+        render();
+    });
+
+
 })
+
+
+
 
 const aboutList = () => {
     $('.aboutList').css('display', 'block');
@@ -11,15 +35,3 @@ const closeAboutList = () => {
     $('.aboutList').css('display', 'none');
 }
 
-const list = [];
-
-$('form').on('submit', (event) => {
-  const inputValue = $('#input-box').val();
-
-  list.push( inputValue );
-
-  event.preventDefault();
-  $(event.currentTarget).trigger('reset');
-
-  render();
-});
